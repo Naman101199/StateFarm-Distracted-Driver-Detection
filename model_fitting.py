5fc07039295375d506ad0aafcc980ca12d4b13fe
Naman Mehta <58945986+Naman101199@users.noreply.github.com>
2020-03-27 15:36:46 +0530

    Update model_fitting.py

--- model_fitting.py
+++ model_fitting.py
@@ -49,10 +49,44 @@ model.compile(loss="sparse_categorical_crossentropy",
 				metrics=["accuracy"])
 
 
-history = model.fit(X, y, batch_size=32, epochs=10, validation_split=0.1)
+model.fit(X, y, batch_size=32, epochs=10, validation_split=0.1)
 
 model.save('my_model.h5')
 
+#INCEPTION_V3
+	  
+from keras.applications.inception_v3 import InceptionV3
+from keras.preprocessing import image
+from keras.models import Model
+from keras.layers import Dense, GlobalAveragePooling2D
+from keras import backend as K
+from keras.optimizers import SGD
+
+base_model = InceptionV3(weights='imagenet', include_top=False,input_shape=features.shape[1:])
+
+x = base_model.output
+x = GlobalAveragePooling2D()(x)
+
+x = Dense(1024, activation='relu')(x)
+predictions = Dense(10, activation='softmax')(x)
+
+model = Model(inputs=base_model.input, outputs=predictions)
+	  
+# we chose to train the top 2 inception blocks, i.e. we will freeze
+# the first 249 layers and unfreeze the rest:
+for layer in model.layers[:249]:
+   layer.trainable = False
+for layer in model.layers[249:]:
+   layer.trainable = True
+	  
+model.compile(optimizer=SGD(lr=0.0001, momentum=0.9), loss='categorical_crossentropy')
+
+# we train our model again (this time fine-tuning the top 2 inception blocks
+# alongside the top Dense layers
+model.fit(features,pd.get_dummies(labels),batch_size = 32, epochs=40, validation_split=0.2,shuffle = True)	  
+
+model.save('Inception_v3.h5')
+	  
 # Printing a graph showing the accuracy changes during the training phase
 print(history.history.keys())
 plt.figure(1)
